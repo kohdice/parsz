fb4038a2effa06a71199633ee471ffafbb7147c0
kohdice <kohdice@gmail.com>
2025-12-18 16:35:13 +0900

    chore: Add example with build step

--- build.zig
+++ build.zig
@@ -18,4 +18,22 @@ pub fn build(b: *std.Build) void {
 
     const test_step = b.step("test", "Run library tests");
     test_step.dependOn(&run_lib_tests.step);
+
+    // --- examples ---
+    const examples_step = b.step("examples", "Build examples");
+
+    const exe = b.addExecutable(.{
+        .name = "sample",
+        .root_module = b.createModule(.{
+            .root_source_file = b.path("examples/sample.zig"),
+            .target = target,
+            .optimize = optimize,
+            .imports = &.{
+                .{ .name = "parsz", .module = mod },
+            },
+        }),
+    });
+
+    const install_exe = b.addInstallArtifact(exe, .{});
+    examples_step.dependOn(&install_exe.step);
 }
