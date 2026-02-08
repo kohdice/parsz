2a8717c409421abe73e0615ddbbf2c880ca1b901
kohdice <k@kohdice.com>
2026-02-08 11:39:45 +0900

    chore: Update flake.nix

--- flake.nix
+++ flake.nix
@@ -3,6 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
     zig-overlay.url = "github:mitchellh/zig-overlay";
   };
 
@@ -10,45 +11,29 @@
     {
       self,
       nixpkgs,
+      flake-utils,
       zig-overlay,
       ...
     }:
-    let
-      supportedSystems = [
-        "x86_64-linux"
-        "aarch64-linux"
-        "x86_64-darwin"
-        "aarch64-darwin"
-      ];
-
-      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
-
-      nixpkgsFor = forAllSystems (
-        system:
-        import nixpkgs {
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
           inherit system;
           overlays = [ zig-overlay.overlays.default ];
-        }
-      );
-    in
-    {
-      devShells = forAllSystems (
-        system:
-        let
-          pkgs = nixpkgsFor.${system};
-          zig = pkgs.zigpkgs."0.15.2";
-        in
-        {
-          default = pkgs.mkShell {
-            name = "parsz";
+        };
+        zig = pkgs.zigpkgs."0.15.2";
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          name = "parsz";
 
-            nativeBuildInputs = [
-              zig
-            ];
-          };
-        }
-      );
+          nativeBuildInputs = with pkgs; [
+            zig
+          ];
+        };
 
-      formatter = forAllSystems (system: nixpkgsFor.${system}.nixfmt-rfc-style);
-    };
+        formatter = pkgs.nixfmt-rfc-style;
+      }
+    );
 }
